76da05020c745d6a1f97b97ebb379f786eb6dbe8
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2021-05-30 21:57:23 -0500

    finished form

--- src/components/formulario.jsx
+++ src/components/formulario.jsx
@@ -3,17 +3,28 @@ import React, {Fragment,useState } from 'react'
 const Formulario = () => {
   const [nombre, setNombre] = useState('')
   const [edad, setEdad] = useState('')
+  const Validar = (event) => {
+    event.preventDefault()
+    if(!nombre.trim()){
+      console.log('el nombre está vacio')
+    }
+    if(!edad.trim()){
+      console.log('edad está vacia')
+    }
+  }
   return(
     <div className="container">
-      <form className="form-group">
+      <form onSubmit={Validar} className="form-group">
         <input placeholder="tu nombre"
                 className="form-control"
                 type="text"
-                onChange={()=> {}}
+                onChange={(e)=> {setNombre(e.target.value) }}
                 />
         <input placeholder="tu edad"
                 className="form-control mt-2"
-                type="text"/>
+                type="text"
+                onChange={(e)=> {setEdad(e.target.value) }}
+                />
         <input className="btn btn-info mt-2"
                 type="submit"/>
 
